f23d20f9125f63d51b0f2bd5b0706453bda8d277
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-09-01 05:36:44 -0300

    v0.01: Creacion de Tienda de Computadoras

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+class Producto {
+
+  constructor(nombre, categoria, memoria, precio) {
+    this.nombre = nombre;
+    this.categoria = categoria;
+    this.memoria = memoria
+    this.precio = precio;
+  }
+
+  getPrecio() {
+    return this.precio;
+  }
+}
+
+let producto1 = new Producto('RTX 3070', 'Tarjeta Gráfica', '8GB', 312801);
+let producto2 = new Producto('SPECTRIX', 'Memoria Ram', '8GB', 8124);
+let producto3 = new Producto('KINGSTON', 'SSD', '240GB', 4289);
+
+
+function elegir() {
+  let producto = prompt('¿De cual quieres saber el precio?: RTX 3070, SPECTRIX, KINGSTON');
+  return producto;
+}
+
+let articulo = elegir();
+
+function mostrar(nombre, mercancia) {
+  if (articulo == nombre) {
+    alert('El precio es: ' + mercancia.getPrecio());
+  }
+}
+
+mostrar('RTX 3070', producto1);
+mostrar('SPECTRIX', producto2);
+mostrar('KINGSTON', producto3);
