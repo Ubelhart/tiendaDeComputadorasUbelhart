27e6ab607bf8ac2c81d194665909082c9d1f9119
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-09-05 22:18:11 -0300

    v0.03: Agregando Array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/*class Producto {
+class Producto {
 
   constructor(nombre, categoria, memoria, precio) {
     this.nombre = nombre;
@@ -12,13 +12,35 @@
   }
 }
 
-let producto1 = new Producto('RTX 3070', 'Tarjeta Gráfica', '8GB', 312801);
-let producto2 = new Producto('SPECTRIX', 'Memoria Ram', '8GB', 8124);
-let producto3 = new Producto('KINGSTON', 'SSD', '240GB', 4289);
+let productos = [];
 
+productos[0] = new Producto('RTX 3070', 'Tarjeta Gráfica', '8GB', 312801);
+productos[1] = new Producto('SPECTRIX', 'Memoria Ram', '8GB', 8124);
+productos[2] = new Producto('KINGSTON', 'SSD', '240GB', 4289);
+
+const ordenarPrecios = productos.sort(function (productoUno, productoDos) {
+
+  if (productoUno.precio > productoDos.precio) {
+    return 1;
+
+  } else if (productoUno.precio < productoDos.precio) {
+    return -1;
+
+  }
+  return 0;
+
+});
+
+alert('Los precios ordenados de menor a mayor son: ');
+
+for (const producto of productos) {
+  alert(producto.nombre + ': ' + producto.precio);
+
+}
 
 function elegir() {
-  let producto = prompt('¿De cual quieres saber el precio?: RTX 3070, SPECTRIX, KINGSTON');
+  let producto = prompt('¿De cual quieres saber el precio?: RTX 3070, SPECTRIX, KINGSTON').toUpperCase();
+
   return producto;
 }
 
@@ -30,6 +52,9 @@ function mostrar(nombre, mercancia) {
   }
 }
 
-mostrar('RTX 3070', producto1);
-mostrar('SPECTRIX', producto2);
-mostrar('KINGSTON', producto3);*/
+
+
+mostrar('RTX 3070', productos[2]);
+mostrar('SPECTRIX', productos[1]);
+mostrar('KINGSTON', productos[0]);
+
