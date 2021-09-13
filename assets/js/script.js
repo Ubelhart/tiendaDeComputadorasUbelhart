315ac218b2b345ccab5eb0ee701383d1b838835f
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-09-13 06:23:43 -0300

    v0.04: Creacion de secciones con DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 class Producto {
-
-  constructor(nombre, categoria, memoria, precio) {
+  constructor(nombre, imagen, categoria, memoria, precio) {
     this.nombre = nombre;
+    this.imagen = imagen;
     this.categoria = categoria;
-    this.memoria = memoria
+    this.memoria = memoria;
     this.precio = precio;
   }
 
@@ -14,47 +14,45 @@ class Producto {
 
 let productos = [];
 
-productos[0] = new Producto('RTX 3070', 'Tarjeta Gráfica', '8GB', 312801);
-productos[1] = new Producto('SPECTRIX', 'Memoria Ram', '8GB', 8124);
-productos[2] = new Producto('KINGSTON', 'SSD', '240GB', 4289);
+productos.push(
+  new Producto(
+    "RTX 3070",
+    "./media/img/productoUno.webp",
+    "Tarjeta Gráfica",
+    "8GB",
+    312801
+  )
+);
+productos.push(
+  new Producto(
+    "SPECTRIX",
+    "./media/img/productoDos.webp",
+    "Memoria Ram",
+    "8GB",
+    8124
+  )
+);
+productos.push(
+  new Producto(
+    "KINGSTON",
+    "./media/img/productoTres.webp",
+    "SSD",
+    "240GB",
+    4289
+  )
+);
 
-const ordenarPrecios = productos.sort(function (productoUno, productoDos) {
-
-  if (productoUno.precio > productoDos.precio) {
-    return 1;
-
-  } else if (productoUno.precio < productoDos.precio) {
-    return -1;
-
-  }
-  return 0;
-
-});
-
-alert('Los precios ordenados de menor a mayor son: ');
+let main = document.getElementById("main");
 
 for (const producto of productos) {
-  alert(producto.nombre + ': ' + producto.precio);
-
+  let section = document.createElement("section");
+  section.innerHTML = `<h2> ${producto.nombre}</h2>
+  <div class="img-container">
+    <img src="${producto.imagen}" alt="${producto.categoria}">
+  </div>
+  <div class="price">
+    <input type="button" value="Agregar">
+    <p>$${producto.precio}</p>
+  </div>`;
+  main.appendChild(section);
 }
-
-function elegir() {
-  let producto = prompt('¿De cual quieres saber el precio?: RTX 3070, SPECTRIX, KINGSTON').toUpperCase();
-
-  return producto;
-}
-
-let articulo = elegir();
-
-function mostrar(nombre, mercancia) {
-  if (articulo == nombre) {
-    alert('El precio es: ' + mercancia.getPrecio());
-  }
-}
-
-
-
-mostrar('RTX 3070', productos[2]);
-mostrar('SPECTRIX', productos[1]);
-mostrar('KINGSTON', productos[0]);
-
