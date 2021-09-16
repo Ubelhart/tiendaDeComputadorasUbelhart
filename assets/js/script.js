197159c416ecad74799c0be0c6a08354e9a212f0
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-09-15 22:34:27 -0300

    v0.05: Carrito Agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 class Producto {
-  constructor(nombre, imagen, categoria, memoria, precio, stock) {
+  constructor(id, nombre, imagen, categoria, precio) {
+    this.id = id;
     this.nombre = nombre;
     this.imagen = imagen;
     this.categoria = categoria;
-    this.memoria = memoria;
     this.precio = precio;
-    this.stock = stock;
   }
 
   getPrecio() {
@@ -14,104 +13,119 @@ class Producto {
 }
 
 let productos = [];
-let productosCarrito;
+let carrito = [];
+let total = 0;
+let DOMitems = document.getElementById("items");
+let DOMbutton = document.getElementsByClassName("btn-agr");
+let DOMcarrito = document.getElementById("carrito");
+let DOMbotonCarrito = document.getElementById("btn-fin");
+let DOMtotal = document.getElementById("total");
 
 productos.push(
   new Producto(
+    1,
     "RTX 3070",
     "./media/img/productoUno.webp",
     "Tarjeta Gráfica",
-    "8GB",
-    312801,
-    1
+    312801
   )
 );
 productos.push(
   new Producto(
+    2,
     "SPECTRIX",
     "./media/img/productoDos.webp",
     "Memoria Ram",
-    "8GB",
-    8124,
-    1
+    8124
   )
 );
 productos.push(
-  new Producto(
-    "KINGSTON",
-    "./media/img/productoTres.webp",
-    "SSD",
-    "240GB",
-    4289,
-    1
-  )
+  new Producto(3, "KINGSTON", "./media/img/productoTres.webp", "SSD", 4289)
 );
 productos.push(
   new Producto(
+    4,
     "XIGMATEK",
     "./media/img/productoCuatro.webp",
     "Gabinete",
-    "Ninguna",
-    15256,
-    1
+    15256
   )
 );
 productos.push(
   new Producto(
+    5,
     "i9-10850K",
     "./media/img/productoCinco.webp",
     "Procesador",
-    "Ninguna",
-    59999,
-    1
+    59999
   )
 );
 productos.push(
   new Producto(
+    6,
     "ASUS ROG STRIX B550-F",
     "./media/img/productoSeis.webp",
     "Gabinete",
-    "Ninguna",
-    28399,
-    1
+    28399
   )
 );
 
-let main = document.getElementById("main");
-
-function agregarProductos() {
+function dibujarProductos(productos) {
   for (let i = 0; i < productos.length; i++) {
-    if (productos[i].stock > 0) {
-      let section = document.createElement("section");
-      section.innerHTML = `<h2> ${productos[i].nombre}</h2>
-        <div class="img-container">
+    let section = document.createElement("section");
+    section.innerHTML = `<h2> ${productos[i].nombre}</h2>
+        <div>
           <img src="${productos[i].imagen}" alt="${productos[i].categoria}">
         </div>
         <div class="price">
-          <input type="button" value="Agregar">
+          <input class="btn btn-agr" type="button" value="Agregar">
           <p>$${productos[i].precio}</p>
         </div>`;
 
-      main.appendChild(section);
-    }
+    items.appendChild(section);
+  }
+  agregarAlCarrito();
+}
+
+function dibujarCarrito() {
+  for (let i = 0; i < carrito.length; i++) {
+    let section = document.createElement("section");
+    section.innerHTML = `<h3> ${carrito[i].nombre}</h3>
+        <div>
+          <img src="${carrito[i].imagen}" alt="${carrito[i].categoria}">
+        </div>
+        <p>$${carrito[i].precio}</p>`;
+
+    DOMcarrito.appendChild(section);
   }
 }
 
-agregarProductos();
-
-function recargarProductos() {
-  main.textContent = " ";
-  agregarProductos();
-  productosCarrito = productos.filter((s) => s.stock < 1);
+function calcularTotal() {
+  for (let i = 0; i < carrito.length; i++) {
+    total = total + carrito[i].precio;
+  }
 }
 
-let button = document.getElementsByTagName("input");
-
-for (let i = 0; i < productos.length; i++) {
-  button[i].onclick = () => {
-    productos[i].stock--;
-    if (productos[i].stock < 0) {
-      recargarProductos();
-    }
+function finalizarCompra() {
+  DOMbotonCarrito.onclick = () => {
+    carrito.pop();
+    total = 0;
+    DOMtotal.innerText = "$";
+    DOMcarrito.innerHTML = "<h3>Su Compra se ha Completado Exitosamente</h3>";
   };
 }
+
+function agregarAlCarrito() {
+  for (let i = 0; i < DOMbutton.length; i++) {
+    DOMbutton[i].onclick = () => {
+      carrito.push(productos[i]);
+      DOMcarrito.textContent = " ";
+      dibujarCarrito();
+      calcularTotal();
+      DOMtotal.innerText = "$" + total;
+      finalizarCompra();
+    };
+  }
+}
+
+dibujarProductos(productos);
